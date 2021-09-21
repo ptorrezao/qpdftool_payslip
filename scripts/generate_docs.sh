cae9c0e600c52ff26973453fbe145693a769eee1
Pedro Torrezão <pedro.torrezao@gmail.com>
2021-09-21 21:44:30 +0100

    Encrypt the document again

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,16 +1,19 @@
 #/bin/bash
-
+mkdir /work/processed_docs
 echo Checking docs
 arr=()
 for f in  /work/docs/*.pdf; do 
    filename=${f#"/work/docs/"};
    arr+=($filename)
-   qpdf --password=$secredPassword --decrypt /work/docs/$filename /work/processed_docs/$filename 
+   qpdf --password=$secretPassword --decrypt /work/docs/$filename /work/processed_docs/$filename 
 done
 echo "List of ${#arr[@]} documents (${arr[@]})"
 now=$(date +"%Y%m%d")
 echo Merging docs $now.pdf will be the output
-qpdf --empty --pages $(for i in /work/processed_docs/*.pdf; do echo $i 1-z; done) -- /work/output/$now.pdf
+qpdf --empty --pages $(for i in /work/processed_docs/*.pdf; do echo $i 1-z; done) -- /work/output/clear_$now.pdf
+
+qpdf --encrypt "$secretPassword" "$secretPassword" 40 --print=n -- /work/output/clear_$now.pdf /work/output/$now.pdf
 echo Output produced with Success
 rm /work/processed_docs/*
+rm /work/output/clear_$now.pdf
 echo Cleaning the folders
